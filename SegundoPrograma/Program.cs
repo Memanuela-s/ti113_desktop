f6dbda027e38ff0f00359540246d78bcc22cda71
Memanuela-s <mayara.esilva6@senacsp.edu.br>
2025-02-19 19:16:08 -0300

    feat:correcao

---
+++ SegundoPrograma/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Segundo_Programa
+{
+    internal class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("informe um numero de 1 a 7 para exibir o Dia da semana");
+            string opcao = Console.ReadLine()!;
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("domingo");
+                    break;
+                case "2":
+                    Console.WriteLine("Segunda-feira");
+                    break;
+                case "3":
+                    Console.WriteLine("terça-feira");
+                    break;
+                case "4":
+                    Console.WriteLine("quinta-feira");
+                    break;
+                case "5":
+                    Console.WriteLine("quarta-feira");
+                    break;
+                case "6":
+                    Console.WriteLine("sexta-feira");
+                    break;
+                case "7":
+                    Console.WriteLine("sabado");
+                    break;
+                default:
+                    Console.WriteLine("Dia Invalido");
+                    break;
+            }
+        }
+    }
+}
---
+++ TerceiroPrograma/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o 1° Número: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            Console.WriteLine("Digite o 2° Número: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            Console.Write($"O maior número é {Math.Max(numero1, numero2)}");
+        }
+    }
+}
